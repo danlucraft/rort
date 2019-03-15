b32fa841f34a78c56262df3dec39a516426af8e6
Dan Lucraft <daniel.lucraft@fundingcircle.com>
2019-03-15 15:13:58 +0000

    Very stuck here on whether to use Vec<String> or Vec<&str>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use std::io;
+use std::env;
+use std::fs;
 
 fn main() {
-    let mut lines: Vec<String> = vec![];
-    loop {
-        let mut line = String::new();
-        match io::stdin().read_line(&mut line) {
-            Ok(0) => break,
-            Ok(_) => lines.push(line),
-            Err(error) => {
-                println!("error: {}", error);
-                ::std::process::exit(1);
-            }
-        }
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let mut lines: Vec<&str>;
+    if args.len() > 1 {
+        let filename = &(args[1]);
+        println!("loading {}", filename);
+        lines = lines_from_file(filename);
+    } else {
+        lines = lines_from_stdin();
     }
     lines.sort();
     for line in lines {
@@ -21,4 +21,33 @@ fn main() {
             println!("{}", line);
         }
     }
+}
+
+fn lines_from_stdin() -> Vec<&'static str> {
+    let mut lines: Vec<&str> = vec![];
+    loop {
+        let mut line: String = String::new();
+        match io::stdin().read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => lines.push(&'static line),
+            Err(error) => {
+                println!("error: {}", error);
+                ::std::process::exit(1);
+            }
+        }
+    }
+    lines
+}
+
+fn lines_from_file(filename: &str) -> Vec<&'static str> {
+    match fs::read_to_string(filename) {
+        Ok(contents) => {
+            return contents.lines().collect()
+        }
+        Err(error) => {
+            println!("error: {}", error);
+            ::std::process::exit(1);
+        }
+    }
+    
 }
--- test.rb
+++ test.rb
@@ -1,8 +1,10 @@
 def test(file)
   `cat #{file} | ./target/debug/rort > tmp/test.rort`
+  `./target/debug/rort #{file} > tmp/test.rort.2`
   `cat #{file} | sort                > tmp/test.sort`
-  results = %x[diff tmp/test.rort tmp/test.sort].split("\n")
-  p results
+  results = %x(diff tmp/test.rort tmp/test.sort).split("\n")
+  results2 = %x(diff tmp/test.rort.2 tmp/test.sort).split("\n")
+  p results + results2
 end
 
 test("Cargo.toml")
